e19056f9ce9766847cf47fa14e4bd370934a4f18
Neto test <xmag91@mail.ru>
2024-06-04 17:17:30 +0300

    New fixed 50 line

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -47,7 +47,7 @@ class CallbackTest {
         elements.get(1).sendKeys("+79270000000");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("[data-test-id=order-success]")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
@@ -57,9 +57,19 @@ class CallbackTest {
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+78005553535");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("button.button")).click();
-        var actualTextElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        var actualTextElement = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
         var actualText = actualTextElement.getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
         assertTrue(actualTextElement.isDisplayed());
+    }
+
+    @Test
+    void SendingFormTest() throws InterruptedException {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алексей Бабкин");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79102436802");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
     }
 }
